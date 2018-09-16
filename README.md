63ecb4a2d36bdfbd62b58b5c917f66034189bbd4
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-09-16 11:42:12 +0200

    apk added

---
+++ README.md
@@ -0,0 +1,3 @@
+# MunichWays
+
+Install with: 
--- build/android/assets/getCalendar.js
+++ build/android/assets/getCalendar.js
@@ -1,11 +1,11 @@
+const XMLTools=require('libs/xmltools');
+
 module.exports=function(cb){
 const URL='https://www.munichways.com/?plugin=all-in-one-event-calendar&controller=ai1ec_exporter_controller&action=export_events&xml=true',
 $=Ti.Network.createHTTPClient({
 onload:function(e){
 const json=null,
 xml=this.responseText;
-console.log(xml);
-const XMLTools=require('libs/xmltools');
 try{
 json=new XMLTools(this.responseText).toJSON(),
 console.log(json);
@@ -16,6 +16,6 @@ console.log(E);
 }});
 
 $.open('GET',URL),
-$.setRequestHeader('Accept','text/html,application/xhtml,application/xml'),
+$.setRequestHeader('Accept','text/html,text/xml,application/xml'),
 $.send();
 };
