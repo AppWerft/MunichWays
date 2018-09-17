b76813ea9431aa68ff44cbc1993b21b20a3382cb
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-09-17 13:30:56 +0200

    progress

--- build/android/assets/control/routes.js
+++ build/android/assets/control/routes.js
@@ -5,8 +5,6 @@ var that=this;
 require('data/routes').forEach(function(item,iindex){
 const GEO=JSON.parse(Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,'data','geojson',item.file+'.geojson').read().getText());
 GEO.features.forEach(function(f,findex){
-console.log('Feature '+iindex+' '+findex),
-console.log(f);
 const points=f.geometry.coordinates.map(function(c){
 return{
 latitude:c[1],
@@ -16,6 +14,11 @@ longitude:c[0]};
 1<points.length&&
 that.routes.push({
 points:points,
+id:iindex+'_'+findex,
+meta:{
+name:f.properties.Name,
+description:f.properties.description},
+
 color:item.color});
 
 
