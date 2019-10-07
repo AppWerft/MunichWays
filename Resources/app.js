a2791715ee4f06e9f1dbc55be644bd2876844d0d
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-10-07 13:38:50 +0200

    landscape mode without actionbar, official velomap added

--- Resources/app.js
+++ Resources/app.js
@@ -10,16 +10,24 @@ function Log(foo) {
 	t = new Date().getTime();
 }
 
+const LAYERID = "6ddc01f1@9b6953ac45e3de3050693c3b1a21a83d:1570390392755";
+const LHM = "https://cartocdn-gusc-c.global.ssl.fastly.net/usocialmaps/api/v1/map/usocialmaps@" + LAYERID + "/1,2,3,4,5/{z}/{x}/{y}.png";
+const OPENPT = "http://openptmap.org/tiles/{z}/{x}/{y}.png";
+
 var mock = false;
 
 (function() {
 
-	var $ = Ti.UI.createWindow({
+const $ = Ti.UI.createWindow({
 		exitOnClose : true,
-		geolocation : false
-	});
-	var focused = false;
+		geolocation : false,
+		_Polylines : []
 
+	});
+
+	var focused = false;
+	$.addEventListener('open', require('onOpen'));
+	$.open();
 	$.mapView = TiMap.createView({
 		userLocationButton : true,
 		userLocation : Ti.Geolocation.locationServicesEnabled ? true : false,
@@ -40,35 +48,35 @@ var mock = false;
 	//$.add($.hintView);
 
 	var Routes = new RouteModule();
-	const URLS = ["https://www.munichways.com/App/radlvorrangnetz.geojson", "https://www.munichways.com/App/gesamtnetz.geojson"]
+	const URLS = ["https://www.munichways.com/App/radlvorrangnetz.geojson", "https://www.munichways.com/App/gesamtnetz.geojson"];
 	URLS.forEach(function(url) {
 		Routes.getPolylines(TiMap, url, function(polylines) {
+			$._Polylines.push(polylines);
 			$.mapView.addPolylines(polylines);
 		});
 
 	});
 
-	$.addEventListener('open', require('onOpen'));
-
-	$.radPin = TiMap.createAnnotation({
-		image : '/images/rad.png',
-		latitude : STACHUS[0],
-		visible : Ti.Geolocation.locationServicesEnabled ? true : false,
-		longitude : STACHUS[1]
-	});
-
 	$.dummyPin = TiMap.createAnnotation({
 		image : '/images/dummy.png',
 		latitude : STACHUS[0],
-
 		longitude : STACHUS[1]
 	});
 	$.mapView.addEventListener('complete', function() {
-		$.mapView.addAnnotation($.radPin);
 		$.mapView.addAnnotation($.dummyPin);
+		$._LHM = TiMap.createTileOverlay({
+			debuglevel : 2,
+			service : TiMap.TILE_OVERLAY_TYPE_XYZ,
+			url : LHM
+		});
+		$._OpenPT = TiMap.createTileOverlay({
+			debuglevel : 1	,
+			service : TiMap.TILE_OVERLAY_TYPE_XYZ,
+			name : 'OpenPtMap'
+		});
 	});
 	$.mapView.addEventListener('click', function(e) {
-		
+
 		$.dummyPin.latitude = e.latitude;
 		$.dummyPin.longitude = e.longitude;
 		$.dummyPin.title = e.source.name;
@@ -87,7 +95,42 @@ var mock = false;
 		focused = false;
 
 	});
+	$.toggleOpenPT = function(on) {
+		switch (on) {
+		case true:
+			$._OpenPT && $.mapView.addTileOverlay($._OpenPT);
+			break;
+		case false:
+			$._OpenPT && $.mapView.removeTileOverlay($._OpenPT);
+			break;
+		}
+	};
+	$.toggleLHM = function(on) {
+		switch (on) {
+		case true:
+			$._LHM && $.mapView.addTileOverlay($._LHM);
+			break;
+		case false:
+			$._LHM  && $.mapView.removeTileOverlay($._LHM);
+			break;
+		}
+	};
+	$.toggleMunichWays = function(on) {
+		switch (on) {
+		case true:
+			$._Polylines.forEach(function(p) {
+				$.mapView.addPolylines(p);
+			});
 
-	$.open();
+			break;
+		case false:
+			$._Polylines.forEach(function(p) {
+				$.mapView.removeAllPolylines();
+			});
+			break;
+
+		}
+	};
+
 })();
 
--- Resources/onOpen.js
+++ Resources/onOpen.js
@@ -1,22 +1,53 @@
 const abx = require('com.alcoapps.actionbarextras');
 
-module.exports= function(e) {
-	const $ = e.source;
+module.exports = function(event) {
+	const $ = event.source;
+	const onMenuItem1click = function(e) {
+		e.source.checked = !e.source.checked;
+		$.toggleMunichWays(e.source.checked);
+	};
+	const onMenuItem2click = function(e) {
+		e.source.checked = !e.source.checked;
+		$.toggleOpenPT(e.source.checked);
+	};
+	const onMenuItem3click = function(e) {
+		e.source.checked = !e.source.checked;
+		$.toggleLHM(e.source.checked);
+	};
 	$.activity.onCreateOptionsMenu = function(e) {
 		abx.backgroundColor = '#6699cc';
-		abx.subtitle = "Radlwege in München";
+		abx.subtitle = "Mit dem Rad sicher und gemütlich durch München auf breiten Radwegen";
 		abx.statusbarColor = '#6699cc';
 		var menu = e.menu;
-		const menuItem1 = menu.add({
+		const menuItem0 = menu.add({
 			title : 'Web',
 			icon : '/images/web.png',
 			showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM | Ti.Android.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW
 		});
-		
-		menuItem1.addEventListener('click', require('/web.window'));
+
+		const menuItem1 = menu.add({
+			title : 'Gesamt-Radlnetz',
+			checkable : true,
+			checked : true
+		});
+
+		const menuItem2 = menu.add({
+			title : 'ÖPNV',
+			checkable : true,
+			checked : false
+		});
+		const menuItem3 = menu.add({
+			title : 'Hauptrouten LHM',
+			checkable : true,
+			checked : false
+		});
+		menuItem0.addEventListener('click', require('/web.window'));
+		menuItem1.addEventListener('click', onMenuItem1click);
+		menuItem2.addEventListener('click', onMenuItem2click);
+		menuItem3.addEventListener('click', onMenuItem3click);
 		require('libs/checkPermissions')(['ACCESS_FINE_LOCATION'], {
 			onOK : function(e) {
-				$.geolocation=true;
+				$.geolocation = true;
 				return;
 				Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
 				Ti.Geolocation.distanceFilter = 20;
@@ -29,4 +60,14 @@ module.exports= function(e) {
 			}
 		});
 	};
+	Ti.Gesture.addEventListener("orientationchange", function(orientationchangeEvent) {
+		const actionBar = $.activity.actionBar;
+		if (actionBar) {
+			if ([Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT].indexOf(orientationchangeEvent.orientation) > -1) {
+				actionBar.hide();
+			} else {
+				actionBar.show();
+			}
+		}
+	});
 };
